751626130c93bbd6a959d1964f2e0d8f8e9d1496
László Monda <laci@monda.hu>
2015-01-07 21:24:30 +0100

    Save the TTL value into the config file.

--- udyndnsd.js
+++ udyndnsd.js
@@ -5,12 +5,16 @@ var dns = require('native-dns');
 var express = require('express');
 
 var DEFAULT_HTTP_PORT = 8080;
+var DEFAULT_TTL = 1; // seconds
 
 var config_filename = '/etc/udyndnsd.json';
 var config = JSON.parse(fs.readFileSync(config_filename));
 var domains = config.domains;
 if (!config.httpPort) {
     config.httpPort = DEFAULT_HTTP_PORT;
+}
+if (!config.ttl) {
+    config.ttl = DEFAULT_TTL;
 }
 
 var DNS_PORT = 53;
@@ -22,7 +26,7 @@ dns.createServer().on('request', function(req, res) {
         res.answer.push(dns.A({
             name: hostname,
             address: domains[hostname],
-            ttl: 1
+            ttl: config.ttl
         }));
     }
 
